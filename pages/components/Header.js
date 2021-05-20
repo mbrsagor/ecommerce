5ad9359857ddaccfae1adc252f44dc3a930e73c5
mbrsagor <mbrsagor@gmail.com>
2021-05-21 00:35:46 +0600

    New: tailwindcss implements

---
+++ pages/components/Header.js
@@ -0,0 +1,9 @@
+function Header(){
+    return(
+        <div>
+            <h2>Header</h2>
+        </div>
+    )
+}
+
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,23 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Header from './components/Header'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Facebook || Homepage</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      
+      <Header />
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="/">Facebook</a>
-        </h1>
-
-        <p className={styles.description}>
-          Facebook Clone App
-        </p>
+      <main>
+        {/* Sidebar */}
+        {/* Feed */}
+        {/* Widgets */}
 
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          &copy; 2021 All rights reserved || Develop by <a href="http://github.com/mbrsagor">Sagor</a>
-        </a>
-      </footer>
     </div>
   )
 }
