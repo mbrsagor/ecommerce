68a31c8cfc9b37148db13b59af87c91d5686421c
mbrsagor <mbrsagor@gmail.com>
2021-05-22 23:16:08 +0600

    New: Add new django project

--- pages/components/nextconfig.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    images: {
-      domains: [
-          "dn0.iconfinder.com",
-          "platform-lookaside.fbsbx.com",
-          "firebasestorage.googleapis.com"
-      ],
-    },
-  }
