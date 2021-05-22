58e446b28ce91fdb1d3af535837fb4c50e722b0f
mbrsagor <mbrsagor@gmail.com>
2021-05-22 23:06:58 +0600

    Error: Fixed image error handling

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -1,21 +1,8 @@
-import Image from "next/image";
 
 function Header(){
     return(
         <div>
             <h2>Header</h2>
-            {/* Left */}
-
-            <div>
-                
-                <Image src="https://cdn0.iconfinder.com/data/icons/social-messaging-ui-color-shapes-2-free/128/social-facebook-2019-circle-512.png"
-                alt="Picture of the author"
-                width={50}
-                height={50}/>
-            </div>
-
-            {/* Center */}
-            {/* Right */}
         </div>
     )
 }
