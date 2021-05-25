0da0a859a2399d0dcaac7aa86575e1c16c424cd3
mbrsagor <mbrsagor@gmail.com>
2021-05-25 20:07:49 +0600

    Bug: Resolved models bug

--- accounts/models.py
+++ accounts/models.py
@@ -8,8 +8,9 @@ class BaseModel(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
+
 class UserProfile(BaseModel):
-    user = models.OneToOneField(User, related_name="user")
+    user = models.OneToOneField(User, related_name="user", on_delete=models.CASCADE)
     bio = models.CharField(max_length=120, blank=True, null=True)
     nick_name = models.CharField(max_length=50, blank=True, null=True)
     birthday = models.DateField(blank=True, default='2004-12-01')
@@ -28,22 +29,25 @@ class UserProfile(BaseModel):
 
     def full_name(self):
         return f"{self.user.first_name} {self.user.last_name}"
-    
+
     def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
         self.visible_name = self.get_visible_name()
         self.url = reverse('profile', args=[self.user.username])
-        return super().save(force_insert=force_insert, force_update=force_update, using=using, update_fields=update_fields)
+        return super().save(force_insert=force_insert, force_update=force_update, using=using,
+                            update_fields=update_fields)
 
     def send_friend_request(self, user_profile):
         self.friend_requests.add(user_profile)
-        noti = Notification.objects.create(owner=self, type=Notification.FRIEND_REQUEST, sender=user_profile.user.username)
+        noti = Notification.objects.create(owner=self, type=Notification.FRIEND_REQUEST,
+                                           sender=user_profile.user.username)
         self.notification_set.add(noti)
         return self.friend_requests.count()
 
     def cancel_friend_request(self, user_profile):
         self.friend_requests.remove(user_profile)
         self.notification_set.get(type=Notification.FRIEND_REQUEST, sender=user_profile.user.username).delete()
-        noti = Notification.objects.create(owner=user_profile, type=Notification.DECLINED_FRIEND_REQUEST, sender=self.user.username)
+        noti = Notification.objects.create(owner=user_profile, type=Notification.DECLINED_FRIEND_REQUEST,
+                                           sender=self.user.username)
         user_profile.notification_set.add(noti)
         return self.friend_requests.count()
 
@@ -52,30 +56,33 @@ class UserProfile(BaseModel):
         self.notification_set.get(type=Notification.FRIEND_REQUEST, sender=user_profile.user.username).delete()
         self.friends.add(user_profile)
         user_profile.friends.add(self)
-        noti = Notification.objects.create(owner=user_profile, type=Notification.ACCEPTED_FRIEND_REQUEST, sender=self.user.username)
+        noti = Notification.objects.create(owner=user_profile, type=Notification.ACCEPTED_FRIEND_REQUEST,
+                                           sender=self.user.username)
         user_profile.notification_set.add(noti)
         return self.friends.count()
-    
+
     def remove_friend(self, user_profile):
         self.friends.remove(user_profile)
         user_profile.friends.remove(self)
-        noti = Notification.objects.create(owner=user_profile, type=Notification.REMOVED_FRIEND, sender=self.user.username)
+        noti = Notification.objects.create(owner=user_profile, type=Notification.REMOVED_FRIEND,
+                                           sender=self.user.username)
         user_profile.notification_set.add(noti)
         return self.friends.count()
-    
+
     @staticmethod
     def user_directory_path(instance, filename):
         # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
         return 'user_{0}/{1}'.format(instance.user.id, filename)
-    
+
 
 class UserWall(models.Model):
-    profile = models.OneToOneField(UserProfile)
+    profile = models.OneToOneField(UserProfile, related_name="user_wall", on_delete=models.CASCADE)
     url = models.CharField(blank=True, max_length=256)
 
     def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
         self.url = self.profile.url
-        return super().save(force_insert=force_insert, force_update=force_update, using=using, update_fields=update_fields)
+        return super().save(force_insert=force_insert, force_update=force_update, using=using,
+                            update_fields=update_fields)
 
     def add_note(self, text, author):
         return self.post_set.create(text=text, author=author)
@@ -85,7 +92,7 @@ class Notification(models.Model):
     type = models.CharField(blank=False, max_length=64)
     text = models.CharField(blank=True, max_length=128)
     url = models.CharField(blank=True, max_length=256)
-    owner = models.ForeignKey(UserProfile, blank=False)
+    owner = models.ForeignKey(UserProfile, blank=False, on_delete=models.CASCADE)
     sender = models.CharField(blank=True, max_length=128)
     date = models.DateTimeField(auto_now_add=True)
 
@@ -124,7 +131,8 @@ class Notification(models.Model):
             self.text = self._REMOVED_FRIEND_TEXT.format(sender_profile.visible_name)
             self.url = reverse('friends', kwargs={'username': self.owner.user.username})
 
-        return super().save(force_insert=force_insert, force_update=force_update, using=using, update_fields=update_fields)
+        return super().save(force_insert=force_insert, force_update=force_update, using=using,
+                            update_fields=update_fields)
 
 
 class Likable(models.Model):
@@ -144,11 +152,10 @@ class Likable(models.Model):
 
 class Note(models.Model):
     text = models.CharField(blank=True, max_length=8128)
-    author = models.ForeignKey(UserProfile, related_name='author_%(class)s')
+    author = models.ForeignKey(UserProfile, related_name='nodes', on_delete=models.CASCADE)
     date = models.DateTimeField(auto_now_add=True)
     attachment = models.FileField(upload_to=UserProfile.user_directory_path, blank=True)
     url = models.CharField(blank=True, max_length=256)
 
     class Meta:
         abstract = True
-
