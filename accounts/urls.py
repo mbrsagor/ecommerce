17bd8e4009d09dc0e92e2428893eaf6598aa96f7
mbrsagor <mbrsagor@gmail.com>
2021-07-29 21:59:04 +0600

    New: Profile API created

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
+    path('profile/', views.ProfileAPIView.as_view())
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,17 @@
-from rest_framework import views, status
+from rest_framework import views, status, permissions
 from rest_framework.response import Response
 
 from .models import UserProfile
+from .serializers import UserProfileSerializer
+
+
+class ProfileAPIView(views.APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request, pk):
+        profile = UserProfile.objects.get(id=pk).first()
+        serializer = UserProfileSerializer(profile, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, pk):
+        pass
