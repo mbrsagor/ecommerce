a4d1aa1877c4ecfd3e3352bbb8e9d2f60f81cb2a
mbrsagor <mbrsagor@gmail.com>
2021-07-30 22:28:13 +0600

    Refactor: Profile apiview API created

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import UserProfile
 
@@ -10,3 +11,12 @@ class UserProfileSerializer(serializers.ModelSerializer):
             'friend_requests', 'profile_picture', 'cover_picture', 'created'
         ]
         read_only_fields = ('user',)
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            'id', 'username', 'email', 'first_name', 'last_name', 'is_superuser', 'is_active',
+            'is_staff', 'last_login'
+        ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,48 +1,25 @@
+from django.contrib.auth.models import User
 from rest_framework import views, status, permissions
 from rest_framework.response import Response
 
 from .models import UserProfile
-from .serializers import UserProfileSerializer
+from .serializers import UserProfileSerializer, UserSerializer
 
 
 class ProfileAPIView(views.APIView):
     permission_classes = [permissions.IsAuthenticated]
+    status_code = status.HTTP_200_OK
 
     def get(self, request):
         try:
-            user_profile = UserProfile.objects.get(user=request.user).first()
-            status_code = status.HTTP_200_OK
-            response = {
-                'success': 'true',
-                'status code': status_code,
-                'message': 'User profile fetched successfully',
-                'data': [{
-                    'first_name': user_profile.user.first_name,
-                    'last_name': user_profile.user.last_name,
-                    'username': user_profile.user.username,
-                    'email': user_profile.user.email,
-                    'bio': user_profile.bio,
-                    'birthday': user_profile.birthday,
-                    'town': user_profile.town,
-                    'relationship': user_profile.relationship,
-                    'relation_user': user_profile.relation_user,
-                    'nick_name': user_profile.nick_name,
-                    'friends': user_profile.nick_name,
-                    'friend_requests': user_profile.friend_requests,
-                    'profile_picture': user_profile.profile_picture,
-                    'cover_picture': user_profile.cover_picture,
-                }]
-            }
+            user_profile = UserProfile.objects.get(id=self.request.user.id)
+            serializer = UserProfileSerializer(user_profile)
+            return Response(serializer.data, status=self.status_code)
 
         except Exception as e:
-            status_code = status.HTTP_400_BAD_REQUEST
-            response = {
-                'success': 'false',
-                'status code': status.HTTP_400_BAD_REQUEST,
-                'message': 'User does not exists',
-                'error': str(e)
-            }
-        return Response(response, status=status_code)
+            user = User.objects.get(id=self.request.user.id)
+            serializer = UserSerializer(user)
+            return Response(serializer.data, status=self.status_code)
 
     def put(self, request, pk):
         pass
