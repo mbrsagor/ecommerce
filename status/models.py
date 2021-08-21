208e64a0e5121278264052197465f1cd98f7166d
mbrsagor <mbrsagor@gmail.com>
2021-08-21 10:33:03 +0600

    Bug: Status models post-related bug resovled

--- status/models.py
+++ status/models.py
@@ -21,14 +21,14 @@ class Feeling(BaseModel):
 
 class Post(BaseModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_status')
-    title = models.CharField(default=None, blank=True)
+    title = models.CharField(max_length=100, default=None, blank=True)
     content = models.TextField()
     activity = JSONField(null=True, blank=True, default=None)
     feelings = models.ForeignKey(Feeling, on_delete=models.CASCADE, blank=True, null=True, related_name='post_feeling')
     is_publish = models.BooleanField(default=True)
     is_private = models.BooleanField(default=False)
     is_friends = models.BooleanField(default=False)
-    location = models.ForeignKey(Location, on_delete=models.CASCADE, related_name='location_area', blank=True)
+    select_location = models.ForeignKey(Location, on_delete=models.CASCADE, related_name='location_area', blank=True)
     image = models.ImageField(upload_to='post', blank=True, null=True)
     tag_friends = models.ManyToManyField(User, blank=True, related_name='tag_friend')
 
